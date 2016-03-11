45a8ba50ea792bc90d29cf228133e4b0da215317
gaoyx <kbyyd24@outlook.com>
2016-03-11 20:59:09 +0800

    add annotations

--- src/producer/Producer.java
+++ src/producer/Producer.java
@@ -1,23 +1,37 @@
 package producer;
 
 import java.io.File;
-import java.util.concurrent.BlockingDeque;
 import java.util.concurrent.BlockingQueue;
 
 /**
  * Created by kbyyd on 2016/3/11.
+ * Producer class is used to list files and add them into the blocking queue,
+ * it realized Runnable interface.
+ * @author gaoyx
+ * @version 1.0
  */
 public class Producer implements Runnable {
+    /*a flag in blocking queue*/
     public static File flag = new File("");
 
     private BlockingQueue<File> queue;
     private File rootDir;
 
+    /**
+     * Constructor
+     * @param queue a BlockingQueue object to mark files
+     * @param rootDir the aim dir of the command
+     */
     public Producer (BlockingQueue<File> queue, File rootDir) {
         this.queue = queue;
         this.rootDir = rootDir;
     }
 
+    /**
+     * add files into the blocking queue.
+     * @param dir the dir of the files path
+     * @throws InterruptedException
+     */
     public void addToQueue (File dir) throws InterruptedException {
         File[] files = dir.listFiles();
         for (File file : files) {
